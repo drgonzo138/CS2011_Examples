d2697b9833b7155fef95a1bc814375a756ccec29
K. Knaur <kknaur@gmail.com>
2016-09-12 10:05:55 -0700

    ~KK

--- lecture04/TestBooleanOperators.java
+++ lecture04/TestBooleanOperators.java
@@ -9,14 +9,17 @@ public class TestBooleanOperators {
     System.out.print("Enter an integer: ");
     int number = input.nextInt();
 
-    if (number % 2 == 0 && number % 3 == 0)
+    if (number % 2 == 0 && number % 3 == 0) {
       System.out.println(number + " is divisible by 2 and 3.");
+    }
 
-    if (number % 2 == 0 || number % 3 == 0)
+    if (number % 2 == 0 || number % 3 == 0) {
       System.out.println(number + " is divisible by 2 or 3.");
+    }
 
-    if (number % 2 == 0 ^ number % 3 == 0)
+    if (number % 2 == 0 ^ number % 3 == 0) {
       System.out.println(number + 
         " divisible by 2 or 3, but not both.");
+    }
   }
 }
