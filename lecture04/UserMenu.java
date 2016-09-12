c12cd402b3f1e68e858c9e7d7da9507b967ba68b
K. Knaur <kknaur@gmail.com>
2016-09-12 10:03:49 -0700

    ~KK

---
+++ lecture04/UserMenu.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class UserMenu {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		//You can create the text of the menu and store it in a variable.
+		final String USER_MENU = "1. Add two numbers\n" + 
+					  			 "2. Area of Rectangle\n" +
+		              			 "3. Remainder of two numbers\n" +
+		              			 "4. Exit Program\n" +
+		              			 "Enter (1-4) for choice: ";
+
+		System.out.print(USER_MENU);
+		int choice = in.nextInt();
+
+		if (choice == 1) {
+			System.out.print("Enter two numbers: ");
+			int x = in.nextInt();
+			int y = in.nextInt();
+
+			int sum = x + y;
+
+			System.out.println("The sum of " + x + ", " + y + " is " + sum);
+		}
+		else if(choice == 2) {
+			//code for option 2
+		}
+		else if(choice == 3) {
+			//code for option 3
+		}
+		else if(choice == 4) {
+			System.exit(0);
+		}
+
+
+
+		// System.out.println("1. Add two numbers");
+		// System.out.println("2. Area of Rectangle");
+		// System.out.println("3. Remainder of two numbers");
+		// System.out.println("Enter (1-3) for choice: ");
+
+
+
+
+
+
+
+	}
+}
