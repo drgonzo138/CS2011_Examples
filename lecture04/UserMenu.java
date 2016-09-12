3d5b3e619ee320d3a280ccb71047587c4a2e0041
K. Knaur <kknaur@gmail.com>
2016-09-12 10:51:43 -0700

    ~KK

--- lecture04/UserMenu.java
+++ lecture04/UserMenu.java
@@ -1,43 +1,43 @@
 /*
-	Shows how to create a simple user choice menu.
+   Shows how to create a simple user choice menu.
 */
 
 import java.util.Scanner;
 
 public class UserMenu {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
 
-		//You can create the text of the menu and store it in a variable.
-		//NOTE: the "\n" is an escape character which inserts a new line
-		//  at the end of the string.
-		final String USER_MENU = 
-		   "1. Add two numbers\n" + 
-			"2. Area of Rectangle\n" +
-		   "3. Remainder of two numbers\n" +
-		   "4. Exit Program\n" +
-		   "Enter (1-4) for choice: ";
+      //You can create the text of the menu and store it in a variable.
+      //NOTE: the "\n" is an escape character which inserts a new line
+      //  at the end of the string.
+      final String USER_MENU = 
+         "1. Add two numbers\n" + 
+         "2. Area of Rectangle\n" +
+         "3. Remainder of two numbers\n" +
+         "4. Exit Program\n" +
+         "Enter (1-4) for choice: ";
 
-		System.out.print(USER_MENU);
-		int choice = in.nextInt();
+      System.out.print(USER_MENU);
+      int choice = in.nextInt();
 
-		if (choice == 1) {
-			System.out.print("Enter two numbers: ");
-			int x = in.nextInt();
-			int y = in.nextInt();
+      if (choice == 1) {
+         System.out.print("Enter two numbers: ");
+         int x = in.nextInt();
+         int y = in.nextInt();
 
-			int sum = x + y;
+         int sum = x + y;
 
-			System.out.println("The sum of " + x + ", " + y + " is " + sum);
-		}
-		else if(choice == 2) {
-			//code for option 2
-		}
-		else if(choice == 3) {
-			//code for option 3
-		}
-		else if(choice == 4) {
-			System.exit(0);
-		}
-	}
+         System.out.println("The sum of " + x + ", " + y + " is " + sum);
+      }
+      else if(choice == 2) {
+         //code for option 2
+      }
+      else if(choice == 3) {
+         //code for option 3
+      }
+      else if(choice == 4) {
+         System.exit(0);
+      }
+   }
 }
