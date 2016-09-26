80c83e2b5a1d96339305d6d07605e398bb6d9f67
K. Knaur <kknaur@gmail.com>
2016-09-26 14:58:53 -0700

    ~KK

---
+++ lecture06/StringBasics.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringBasics {
+
+	public static void main(String[] args) {
+		//Declaring a string
+		
+		//Option 1:
+		String s1 = "Hello World!";
+		
+		//Option 2: 
+		String s2 = new String("Hello World");
+	}
+
+}
